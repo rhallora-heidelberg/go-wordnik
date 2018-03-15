1d6ca538414915c996991847d1627bb424f79b4c
Ryan Halloran <rhallora@heidelberg.edu>
2018-03-15 11:07:41 -0400

    Adding structs for "account", "wordList" and "wordLists" endpoints

--- account.go
+++ account.go
@@ -1,1 +1,31 @@
 package wordnik
+
+// AuthenticationToken as defined by the Wordnik API. Needed for user-specific
+// requests.
+type AuthenticationToken struct {
+	Token         string `json:"token"`
+	UserID        int64  `json:"userId"`
+	UserSignature string `json:"userSignature"`
+}
+
+// APITokenStatus as defined by the Wordnik API.
+type APITokenStatus struct {
+	Valid           bool   `json:"valid"`
+	Token           string `json:"token"`
+	ResetsInMillis  int64  `json:"resetsInMillis"`
+	RemainingCalls  int64  `json:"remainingCalls"`
+	ExpiresInMillis int64  `json:"expiresInMillis"`
+	TotalRequests   int64  `json:"totalRequests"`
+}
+
+// User as defined by the Wordnik API.
+type User struct {
+	ID          int64  `json:"id"`
+	Username    string `json:"username"`
+	Email       string `json:"email"`
+	Status      int64  `json:"status"`
+	FaceBookID  string `json:"faceBookId"`
+	UserName    string `json:"userName"`
+	DisplayName string `json:"displayName"`
+	Password    string `json:"password"`
+}
--- wordList.go
+++ wordList.go
@@ -1,1 +1,44 @@
 package wordnik
+
+// WordLists is a container type for WordList, for the purpose of
+// unmarshalling JSON.
+type WordLists struct {
+	WordLists []WordList
+}
+
+// WordList as defined by the Wordnik API.
+type WordList struct {
+	ID                int64  `json:"id,omitempty"`
+	Permalink         string `json:"permalink,omitempty"`
+	Name              string `json:"name,omitempty"`
+	CreatedAt         string `json:"createdAt,omitempty"`
+	UpdatedAt         string `json:"updatedAt,omitempty"`
+	LastActivityAt    string `json:"lastActivityAt,omitempty"`
+	Username          string `json:"username,omitempty"`
+	UserID            int64  `json:"userId,omitempty"`
+	Description       string `json:"description,omitempty"`
+	NumberWordsInList int64  `json:"numberWordsInList,omitempty"`
+	Type              string `json:"type,omitempty"`
+}
+
+// stringValue is provided for convenient JSON marshalling in PostWordListWords.
+type stringValue struct {
+	Word string `json:"word,omitempty"`
+}
+
+// WordListWords is a container type for WordListWord, for the purpose of
+// unmarshalling JSON.
+type WordListWords struct {
+	WordListWords []WordListWord
+}
+
+// WordListWord as defined by the Wordnik API.
+type WordListWord struct {
+	ID                   int64  `json:"id"`
+	Word                 string `json:"word"`
+	Username             string `json:"username"`
+	UserID               int64  `json:"userId"`
+	CreatedAt            string `json:"createdAt"`
+	NumberCommentsOnWord int64  `json:"numberCommentsOnWord"`
+	NumberLists          int64  `json:"numberLists"`
+}
