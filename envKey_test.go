ecb032a52a1ceae6cb5bc5df37ae9e977c7779be
Ryan Halloran <rhallora@heidelberg.edu>
2018-03-19 11:34:20 -0400

    Adding getEnvUserPass() to assist with account/wordList testing

--- envKey_test.go
+++ envKey_test.go
@@ -16,3 +16,21 @@ func getEnvKey() (string, error) {
 
 	return keyVar, errors.New("environment variable WORDNIK_API_KEY not set")
 }
+
+type testUser struct {
+	user, pass string
+}
+
+// Helper function for testing which retrieves a test username and password from
+// the environment variables WORDNIK_TEST_USER and WORDNIK_TEST_PASS. Returns an
+// error if no key is found.
+func getEnvUserPass() (testUser, error) {
+	user := os.Getenv("WORDNIK_TEST_USER")
+	pass := os.Getenv("WORDNIK_TEST_PASS")
+	if user == "" || pass == "" {
+		err := errors.New("environment variable(s) WORDNIK_TEST_USER and/or WORDNIK_TEST_PASS not set")
+		return testUser{user, pass}, err
+	}
+
+	return testUser{user, pass}, nil
+}
